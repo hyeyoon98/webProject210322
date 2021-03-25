bd1b7251cfef23c76c5ea932603ed48c5d56e323
hyeyoon98 <ymcacom665@gmail.com>
2021-03-25 13:18:59 +0900

    clear first project

---
+++ project/html/javascript/move.js
@@ -0,0 +1,15 @@
+function moveWhite() {
+	location.href = "SimpleWhiteInterior.html";
+}
+
+function moveModern() {
+	location.href = "ModernBlackAndWhite.html";
+}
+
+function moveVintage() {
+	location.href = "VintageStyleTone.html";
+}
+
+function moveWoody() {
+	location.href = "WoodyInterior.html";
+}
